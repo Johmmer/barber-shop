0cfbf2daf02621daa1bcc6da28dc109137494053
johmmer <johmmer300@gmail.com>
2025-06-20 03:12:24 -0400

    'actualizar ruta de templates'

--- barber_shop/barber_shop/settings.py
+++ barber_shop/barber_shop/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'barber_shop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['barber-shop/barber_shop/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
