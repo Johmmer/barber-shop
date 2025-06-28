a1e2b9ea93d422bb3ae14be4f805ef73d075a79b
johmmer <johmmer300@gmail.com>
2025-06-28 02:29:11 -0400

    -

--- barber_shop/reservations/admin.py
+++ barber_shop/reservations/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from .models import Service, Barber, Reservation
+from .models import Service, Barber, Hora, Reservation
 # Register your models here.
 
 admin.site.register(Service)
 
 admin.site.register(Barber)
 
-#admin.site.register(Time)
+admin.site.register(Hora)
 
 admin.site.register(Reservation)
