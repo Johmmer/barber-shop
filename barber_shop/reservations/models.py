c6611d128122a63a15db3f7951eae71200a116e6
johmmer <johmmer300@gmail.com>
2025-06-28 01:56:45 -0400

    str

--- barber_shop/reservations/models.py
+++ barber_shop/reservations/models.py
@@ -20,9 +20,9 @@ class Barber(models.Model):
 
 class Time(models.Model):
     start_time = models.TimeField(null=False, blank=False)
-    
+    text = models.CharField(max_length=100, null=False, blank=False)
     def __str__(self):
-        return self.start_time
+        return self.text
 
 class Reservation(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
