4ed739f5c01e3b1bb6f99c88e985b3002b8ed76e
johmmer <johmmer300@gmail.com>
2025-06-28 01:41:31 -0400

    Campo de hora actualizado

--- barber_shop/reservations/models.py
+++ barber_shop/reservations/models.py
@@ -18,11 +18,17 @@ class Barber(models.Model):
     def __str__(self):
         return self.name
 
+class Time(models.Model):
+    start_time = models.TimeField(null=False, blank=False)
+    
+    def __str__(self):
+        return self.start_time
+
 class Reservation(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
     email = models.EmailField(null=False, blank=False)
     date = models.DateField(null=False, blank=False)
-    time = models.TimeField(null=False, blank=False)
+    time = models.ForeignKey(Time, on_delete=models.CASCADE)
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
     barber = models.ForeignKey(Barber, on_delete=models.CASCADE)
     notes = models.TextField(null=True, blank=True)
